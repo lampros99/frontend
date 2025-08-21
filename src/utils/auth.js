a0d48a72709b6d89c93fa940eb563820a0cd1338
lampros alexandris <lamprospatmos19@gmail.com>
2025-08-21 09:36:15 +0300

    feat: registerUser

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,3 +5,14 @@ export const logout = () => {
   window.location.href = '/login';
 };
 
+export const registerUser = async (email, password) => {
+  const res = await fetch('http://localhost:5000/api/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  return res.json();
+};
